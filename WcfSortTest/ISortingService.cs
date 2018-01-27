c267ad3cca7c0a35265826bc960415f62d9dce3b
zdravkoggeorgiev <zdravko.g@me.com>
2018-01-27 15:45:16 +0200

    Made WCF Skeleton methods/contracts

---
+++ WcfSortTest/ISortingService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfSortTest
+{
+    [ServiceContract]
+    public interface ISortingService
+    {
+        /// <summary>
+        /// Begins a client session and returns a guid (globally unique identifier) for the stream
+        /// </summary>
+        /// <returns></returns>
+        [OperationContract]
+        Guid BeginStream();
+
+        /// <summary>
+        /// sends some text upstream; clients can call this API multiple times for the length of a session
+        /// </summary>
+        /// <param name="streamGuid"></param>
+        /// <param name="text"></param>
+        [OperationContract]
+        void PutStreamData(Guid streamGuid, string[] text);
+
+        /// <summary>
+        /// Streams back the data accumulated so far, sorted ascending lexicographically, treating each line of text as a single word.
+        /// </summary>
+        /// <param name="streamGuid"></param>
+        /// <returns></returns>
+        [OperationContract]
+        Stream GetSortedStream(Guid streamGuid);
+
+        /// <summary>
+        /// Ends a client session and closes the stream; subsequent calls to retrieve the stream data must fail.
+        /// </summary>
+        /// <param name="streamGuid"></param>
+        [OperationContract]
+        void EndStream(Guid streamGuid);
+    }
+}
---
+++ WcfSortTest/SortingService.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfSortTest
+{
+    public class SortingService : ISortingService
+    {
+        public Guid BeginStream()
+        {
+            Guid newGuid = Guid.NewGuid();
+            return newGuid;
+        }
+
+        public void EndStream(Guid streamGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream GetSortedStream(Guid streamGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PutStreamData(Guid streamGuid, string[] text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
